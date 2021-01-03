a0f14877d716fea5d12513154744b9473ba1aa35
oriorasajambu <oriorasajambu@gmail.com>
2021-01-04 02:31:52 +0700

    Update Page

---
+++ js/loadpage.js
@@ -0,0 +1,21 @@
+$(function(){
+  $.get("tools/sidebar.html", '', function (data) { $("#content-wrapper").before(data); });
+  $.get("tools/topbar.html", '', function (data) { $(".container-fluid").before(data); });
+  $.get("tools/footer.html", '', function (data) { $("#content").after(data); });
+  $.get("tools/logoutmodal.html", '', function (data) { $("#page-top").after(data); });
+  var split = location.search.replace('?', '').split('&').map(function(val){
+    if(val.length <= 0) return "null";
+    return val.split('=');
+  });
+  if(split != "null"){
+    if(split[0][1]){
+      var pages = split[0][1];
+      $.get("pages/"+pages+".html", '', function (data) {
+        $(".container-fluid").empty();
+        $(".container-fluid").append(data);
+      });
+    }
+    else $.get("pages/404.html", '', function (data) { $(".container-fluid").append(data); });
+  }
+  else $.get("pages/dashboard.html", '', function (data) { $(".container-fluid").append(data); });
+});
